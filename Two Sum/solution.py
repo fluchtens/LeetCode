5e15ee82c07534b930d9454fedb79b4c6d198dd6
Cleazyy <cleazy2002@gmail.com>
2023-09-13 17:03:36 +0200

    update two sum

--- Two Sum/solution.py
+++ Two Sum/solution.py
@@ -2,20 +2,19 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-      result: List[int] = []
+        result: List[int] = []
 
-      for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-          if nums[i] + nums[j] == target:
-            result.append(i)
-            result.append(j)
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    result.append(i)
+                    result.append(j)
 
-      return (result)
+        return (result)
 
 if __name__ == "__main__":
-  solution = Solution()
+    solution = Solution()
 
-  nums: List[int] = [7, 2, 11, 15]
-  target: int = 9
-
-  print(solution.twoSum(nums, target))
+    nums: List[int] = [7, 2, 11, 15]
+    target: int = 9
+    print(solution.twoSum(nums, target))
