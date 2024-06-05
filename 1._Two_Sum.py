876977a414707e38cf825568128912be60e70cb5
Cleazyy <cleazy2002@gmail.com>
2024-06-05 07:53:10 +0200

    add 1 two sum

---
+++ 1._Two_Sum.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        i: int = 0
+        while (i < len(nums)):
+            j: int = i + 1
+            while (j < len(nums)):
+                if (nums[i] + nums[j] == target):
+                    return [i, j]
+                j = j + 1
+            i = i + 1
+        return 0
